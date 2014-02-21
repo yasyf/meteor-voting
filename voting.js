2b87cb4de92be1803458dfafb6cca04512711f2a
Yasyf Mohamedali <yasyf@yasyf.com>
2014-02-20 23:52:31 -0500

    all admin must agree to delete

--- voting.js
+++ voting.js
@@ -1,6 +1,10 @@
 Items = new Meteor.Collection("items");
-var deleters = Array("AbmyphdodrfQSqP2X");
+var deleters = Array("NPToaEvWZWA7hYbfB","bg9rZ8PEZsw2BvNiQ");
 if (Meteor.isClient) {
+
+  Meteor.startup(function () {
+    Meteor.subscribe("all_items");
+  });
 
   Template.item.items = function () {
     return Items.find({},{sort: {votes: -1, dt: -1}}).fetch();
@@ -8,6 +12,22 @@ if (Meteor.isClient) {
 
   Template.i.canDelete = function(owner) {
     return Meteor.userId() && ((owner === Meteor.userId()) || (_.contains(deleters,Meteor.userId())));
+  };
+
+  Template.i.isAdmin = function() {
+    return _.contains(deleters,Meteor.userId());
+  };
+
+  Template.i.isWaitingForDelete = function(dels) {
+    return _.contains(deleters,Meteor.userId()) && dels && dels.length > 0;
+  };
+
+  Template.i.canHelpDelete = function(dels) {
+    return _.contains(deleters,Meteor.userId()) && !_.contains(dels,Meteor.userId());
+  };
+
+  Template.i.deletersRemaining = function(l) {
+    return deleters.length - l;
   };
 
   Template.i.rendered = function() {
@@ -36,8 +56,9 @@ if (Meteor.isClient) {
     
   Template.add.events({
     'keypress #add' : function (e) {
-      if (e.which === 13 && Meteor.user() && !Items.findOne({text: $("#add").val().substring(0,150).toLowerCase()})) {
-        id = Items.insert({text: $("#add").val().substring(0,150).toLowerCase(), votes: 1, owner: Meteor.userId(), voted: Array(Meteor.userId()), colour: randcol(), dt: new Date()});
+      txt = $("#add").val().substring(0,150).toLowerCase().trim();
+      if (e.which === 13 && Meteor.user() && txt.length > 0 && !Items.findOne({text: txt})) {
+        id = Items.insert({text: txt, votes: 1, owner: Meteor.userId(), voted: Array(Meteor.userId()), colour: randcol(), dt: new Date()});
         $("#add").val('');
       }
     }
@@ -50,13 +71,20 @@ if (Meteor.isClient) {
       }
     },
     'click .delbtn' : function (e) {
-      e.stopImmediatePropagation();
-      var del_id = this._id;
-      $('#item_'+del_id).fadeOut(500);
-      del = function(){
-        Items.remove({_id: del_id});
+      i = Items.findOne({_id: this._id})
+      if(i.owner != Meteor.userId() && _.contains(deleters,Meteor.userId()) && !_.contains(i.deleters,Meteor.userId())){
+        Items.update({_id: this._id}, {$push: {deleters: Meteor.userId()}})
+        i = Items.findOne({_id: this._id})
       }
-      setTimeout(del,500);
+      if(i.owner == Meteor.userId() || (_.contains(deleters,Meteor.userId()) && i.deleters && i.deleters.length == deleters.length)){
+        e.stopImmediatePropagation();
+        var del_id = this._id;
+        $('#item_'+del_id).fadeOut(500);
+        del = function(){
+          Items.remove({_id: del_id});
+        }
+        setTimeout(del,500);
+      }
     }
   });
 
@@ -69,6 +97,15 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Meteor.publish("userData", function () {
+        if(_.contains(deleters,this.userId)){
+          return Meteor.users.find();
+        }
+        return Meteor.users.find({_id: this.userId});
+    });
+    Meteor.publish("all_items", function () {
+      return Items.find(); // everything
+    });
     Items.allow({
       insert: function (userId, doc) {
         // the user must be logged in
